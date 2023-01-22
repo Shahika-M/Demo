70899159412d4512b214c0c354ac955c15d1cfd2
Parween <shahika.parween@capgemini.com>
2023-01-22 20:11:25 +0530

    new recent changes

--- product/src/main/java/com/example/product/controller/ProductController.java
+++ product/src/main/java/com/example/product/controller/ProductController.java
@@ -23,6 +23,7 @@ public class ProductController {
 	@PostMapping("/createProduct")
 	public Product addProduct(@RequestBody Product product) {
 		System.out.println("hello world.......");
+		System.out.println("hello world.......");
 		return productRepository.save(product); 
 	}
 	
